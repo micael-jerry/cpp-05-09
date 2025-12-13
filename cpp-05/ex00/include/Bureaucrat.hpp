6bdc774f7ea1bde3938f235eebb303b16246c56f
Micael Jerry <fidimalala.mj@gmail.com>
2025-12-13 05:50:48 +0300

    CPP05 - ex00

---
+++ cpp-05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 19:06:39 by mfidimal          #+#    #+#             */
+/*   Updated: 2025/12/13 05:47:54 by mfidimal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#define BUREAUCRAT_MAX_GRADE 1
+#define BUREAUCRAT_MIN_GRADE 150
+
+#include <string>
+
+class Bureaucrat
+{
+private:
+	std::string _name;
+	int _grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(std::string const &name, int const &grade);
+	Bureaucrat(Bureaucrat const &src);
+
+	~Bureaucrat();
+
+	Bureaucrat &operator=(Bureaucrat const &src);
+
+	std::string getName(void) const;
+	int getGrade(void) const;
+	void incrementGrade(void);
+	void decrementGrade(void);
+
+public:
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw()
+		{
+			return ("Grade too high");
+		};
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw()
+		{
+			return ("Grade too low");
+		};
+	};
+};
+
+#endif
---
+++ cpp-05/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,49 @@
+#include <Bureaucrat.hpp>
+
+Bureaucrat::Bureaucrat() : _name("default_bureaucrat") {}
+
+Bureaucrat::Bureaucrat(std::string const &name, int const &grade) {
+	if (grade < BUREAUCRAT_MAX_GRADE)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > BUREAUCRAT_MIN_GRADE)
+		throw Bureaucrat::GradeTooLowException();
+	this->_name = name;
+	this->_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &src) : _name(src.getName()), _grade(src.getGrade()) {}
+
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &src)
+{
+	if (this == &src)
+		return (*this);
+	this->_name = src.getName();
+	this->_grade = src.getGrade();
+	return (*this);
+}
+
+std::string Bureaucrat::getName(void) const
+{
+	return (this->_name);
+}
+
+int Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+void Bureaucrat::incrementGrade(void)
+{
+	if ((this->_grade - 1) < BUREAUCRAT_MAX_GRADE)
+		throw Bureaucrat::GradeTooHighException();
+	this->_grade--;
+}
+
+void Bureaucrat::decrementGrade(void)
+{
+	if ((this->_grade + 1) > BUREAUCRAT_MIN_GRADE)
+		throw Bureaucrat::GradeTooLowException();
+	this->_grade++;
+}
