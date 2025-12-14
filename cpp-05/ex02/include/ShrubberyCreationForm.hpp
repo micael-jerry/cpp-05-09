52e021c6c88ca81e74f157caf3b45ee4f0a8aecc
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2025-12-14 16:09:42 +0300

    CPP05 ex02 --

---
+++ cpp-05/ex02/include/ShrubberyCreationForm.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 15:52:28 by mfidimal          #+#    #+#             */
+/*   Updated: 2025/12/14 15:55:32 by mfidimal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./AForm.hpp"
+
+class ShrubberyCreationForm: public AForm
+{
+public:
+	ShrubberyCreationForm();
+	ShrubberyCreationForm(std::string name, )
+};
