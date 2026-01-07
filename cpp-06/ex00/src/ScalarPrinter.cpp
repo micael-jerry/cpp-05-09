9fa47543e453262551d3eaebbd1a933c7d5a7372
Micael Jerry <fidimalala.mj@gmail.com>
2026-01-07 21:30:56 +0300

    CPP06

--- cpp-06/ex00/src/ScalarPrinter.cpp
+++ cpp-06/ex00/src/ScalarPrinter.cpp
@@ -6,15 +6,18 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 04:38:50 by mfidimal          #+#    #+#             */
-/*   Updated: 2026/01/05 06:31:03 by mfidimal         ###   ########.fr       */
+/*   Updated: 2026/01/07 21:28:43 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ScalarPrinter.hpp"
 
 #include <cctype>
+#include <cfloat>
 #include <climits>
 #include <iostream>
+#include <ostream>
+#include <string>
 
 void ScalarPrinter::printCharBase(char const &chr) {
   std::cout << "char: " << chr << std::endl
@@ -46,7 +49,35 @@ void ScalarPrinter::printFloatBase(float const &flt) {
             << "double: " << static_cast<double>(flt) << std::endl;
 }
 
-void ScalarPrinter::printDoubleBase(double const &dbl) { (void)dbl; }
+void ScalarPrinter::printDoubleBase(double const &dbl) {
+  std::cout << "char: ";
+  if (dbl < CHAR_MIN || dbl > CHAR_MAX) {
+    std::cout << "impossible";
+  } else if (std::isprint(static_cast<int>(dbl))) {
+    std::cout << static_cast<char>(dbl);
+  } else {
+    std::cout << "Non displayable";
+  }
+  std::cout << std::endl;
+
+  std::cout << "int: ";
+  if (dbl < INT_MIN || dbl > INT_MAX) {
+    std::cout << "impossible";
+  } else {
+    std::cout << static_cast<int>(dbl);
+  }
+  std::cout << std::endl;
+
+  std::cout << "float: " << static_cast<float>(dbl) << "f" << std::endl;
+  if (dbl < FLT_MIN || dbl > FLT_MAX) {
+    std::cout << "impossible";
+  } else {
+    std::cout << static_cast<float>(dbl) << "f";
+  }
+  std::cout << std::endl;
+
+  std::cout << "double: " << dbl << std::endl;
+}
 
 void ScalarPrinter::printInvalidBase() {
   std::cout << "char: " << "impossible" << std::endl
