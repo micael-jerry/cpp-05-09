5a6e45dbc98a3c0943dc914b565a4d9329eb50d1
Micael Jerry <fidimalala.mj@gmail.com>
2026-01-13 19:22:04 +0300

    CPP06 update

--- cpp-06/ex01/src/Serializer.cpp
+++ cpp-06/ex01/src/Serializer.cpp
@@ -6,19 +6,16 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 05:28:21 by mfidimal          #+#    #+#             */
-/*   Updated: 2026/01/12 06:02:52 by mfidimal         ###   ########.fr       */
+/*   Updated: 2026/01/12 21:30:05 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Serializer.hpp"
 
-
 uintptr_t Serializer::serialize(Data *ptr) {
-	(void) ptr;
-	return 0;
+  return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data *Serializer::deserialize(uintptr_t raw) {
-	(void) raw;
-	return NULL;
+  return reinterpret_cast<Data *>(raw);
 }
--- cpp-06/ex01/src/main.cpp
+++ cpp-06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 05:57:10 by mfidimal          #+#    #+#             */
-/*   Updated: 2026/01/12 05:58:42 by mfidimal         ###   ########.fr       */
+/*   Updated: 2026/01/13 19:21:41 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ int main() {
   Data data(42, "Jerry");
 
   uintptr_t raw = Serializer::serialize(&data);
-  Data* ptr = Serializer::deserialize(raw);
+  Data *ptr = Serializer::deserialize(raw);
+
+  std::cout << "Original ptr: " << &raw << std::endl
+            << "Deserialized ptr: " << ptr << std::endl;
 
   std::cout << "ID: " << ptr->getId() << std::endl;
   std::cout << "Name: " << ptr->getName() << std::endl;
