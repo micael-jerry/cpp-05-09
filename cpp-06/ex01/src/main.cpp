ac629ac80dd6f4f599da3f62b02478ca74914434
Micael Jerry <fidimalala.mj@gmail.com>
2026-01-13 20:04:14 +0300

    CPP06 finalize

--- cpp-06/ex01/src/main.cpp
+++ cpp-06/ex01/src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 05:57:10 by mfidimal          #+#    #+#             */
-/*   Updated: 2026/01/13 19:21:41 by mfidimal         ###   ########.fr       */
+/*   Updated: 2026/01/13 20:03:59 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <ostream>
 
 #include "../include/Data.hpp"
 #include "../include/Serializer.hpp"
@@ -21,9 +22,9 @@ int main() {
   uintptr_t raw = Serializer::serialize(&data);
   Data *ptr = Serializer::deserialize(raw);
 
-  std::cout << "Original ptr: " << &raw << std::endl
-            << "Deserialized ptr: " << ptr << std::endl;
-
+  std::cout << "Original ptr: " << &data << std::endl
+            << "Deserialized ptr: " << ptr << std::endl
+            << std::endl;
   std::cout << "ID: " << ptr->getId() << std::endl;
   std::cout << "Name: " << ptr->getName() << std::endl;
 
