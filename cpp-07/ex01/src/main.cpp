6d1aae73eb87e772f5ca4dfcc3926551463150ff
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2026-02-22 19:24:19 +0300

    fix: simplify ex01 cpp7

--- cpp-07/ex01/src/main.cpp
+++ cpp-07/ex01/src/main.cpp
@@ -23,25 +23,23 @@ void print(T const &x) {
 int main(void) {
   std::cout << "=== Test 1: Integer Array ===" << std::endl;
   int intArray[] = {1, 2, 3, 4, 5};
-  int len = sizeof(intArray) / sizeof(int);
 
   std::cout << "Original: ";
-  iter(intArray, len, printInt);
+  iter(intArray, 5, printInt);
   std::cout << std::endl;
 
   std::cout << "Incrementing values..." << std::endl;
-  iter(intArray, len, incrementInt);
+  iter(intArray, 5, incrementInt);
 
   std::cout << "Modified: ";
-  iter(intArray, len, printInt);
+  iter(intArray, 5, printInt);
   std::cout << std::endl << std::endl;
 
   std::cout << "=== Test 2: String Array ===" << std::endl;
   std::string strArray[] = {"Hello", "World", "42", "C++"};
-  int strLen = sizeof(strArray) / sizeof(std::string);
 
   std::cout << "Strings: ";
-  iter(strArray, strLen, printString);
+  iter(strArray, 4, printString);
   std::cout << std::endl << std::endl;
 
   return 0;
