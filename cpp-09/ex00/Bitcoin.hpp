bdc56efdf3951077641a31e74a4c892699c1d7a2
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2026-03-09 19:40:26 +0300

    init CPP09

---
+++ cpp-09/ex00/Bitcoin.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bitcoin.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 18:54:40 by mfidimal          #+#    #+#             */
+/*   Updated: 2026/03/09 19:16:29 by mfidimal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOIN_HPP
+#define BITCOIN_HPP
+
+#include <string>
+
+
+
+std::string *getFileContent();
+
+
+#endif
