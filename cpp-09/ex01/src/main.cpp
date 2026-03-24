eb5187e98992ce459157417879e99736af40414d
Micael Jerry <fidimalala.mj@gmail.com>
2026-03-24 04:37:41 +0300

    init CPP09 ex01

---
+++ cpp-09/ex01/src/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/24 04:25:36 by mfidimal          #+#    #+#             */
+/*   Updated: 2026/03/24 04:37:13 by mfidimal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "../include/RPN.hpp"
+
+int main(int argc, const char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "ERROR: invalid arguments" << std::endl;
+    return 1;
+  }
+  return 0;
+}
